4dc79cec864f19594376206cafd8a7ef7f4c8384
liyifei <liyifei@yuanben.org>
2021-11-22 16:25:09 +0800

    add db folder if not exists

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 echo $1;
 if [ $1 = "init" ];then
 python3 gen_config.py;
+mkdir -p /app/db;
 echo ".open /app/db/delta_dashboard.db" | sqlite3
 else
 if [ ! -e /app/db/delta_dashboard.db ];then
+mkdir -p /app/db;
 echo ".open /app/db/delta_dashboard.db" | sqlite3;
 fi
 python3 gen_web_config.py;
